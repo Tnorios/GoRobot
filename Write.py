c79bf7c1dc0dae96834f8cb588bd5bbec14fcaf7
Gabriel Tenorio <gabrielbtenorio@hotmail.com>
2018-02-05 01:01:52 -0200

    version 0.3

--- Write.py
+++ Write.py
@@ -17,42 +17,47 @@ def write(sales):
     sales.sort()
     print(sales)
     print('')
-    prog = {}
+    progs = {}
     date = time.strftime("%Y-%m-%d")
     DatePath = '/Users/gabrieltenorio/desktop/files//Arquivos-'+date
     if not os.path.exists(DatePath):
         os.makedirs(DatePath)
 
     for i in range(0,(len(sales))):
-        
-        
-        if sales[i][0] in prog:
-            prog[sales[i][0]].append(sales[i])
+        if sales[i][0] in progs:
+            progs[sales[i][0]].append(sales[i])     
         else:
-            prog[sales[i][0]] = [sales[i]]
+            progs[sales[i][0]] = [sales[i]]
         
     
-    for key, value in prog.items():
+    for key, values in progs.items():
         with open(DatePath+'/'+key+'-'+date + '.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, dialect='excel', delimiter=';')
             writer.writerow(['prog_id','time_stamp','order_id','customer_id','currency_symbol','total_price','review_state','review_note','partner_id','tracking_type','update_flag'])
-            for data in value:
-                line = []
-                ProgID = DB.getProgID(key)
-                Fdate  = time.strftime("%Y-%m-%d" + " 10:00:00")
-                if not(isinstance(data[-1], str)):
-                    seconds = (data[1] - 25569) * 86400.0
-                    reviewNote = (datetime.datetime.utcfromtimestamp(seconds))
+            print('')
+            order = []
+            for data in values:         
+                if data[2] in order:
+                    pass
                 else:
-                    reviewNote = data[-1]
-                PartnerId = DB.getPartnerId(ProgID, 'meliuz@zanox.com')
-                line.extend((ProgID, Fdate, str(data[2]), 'meliuz@zanox.com', 'BRL', float(data[3]), 0 ,str(reviewNote),PartnerId,3,4 ))
-                print(line)
-                writer.writerow(line)
+                    order.append(data[2])
+                    line = []
+                    ProgID = DB.getProgID(key)
+                    Fdate  = time.strftime("%Y-%m-%d" + " 10:00:00")
+                    if not(isinstance(data[-1], str)):
+                        seconds = (data[1] - 25569) * 86400.0
+                        reviewNote = (datetime.datetime.utcfromtimestamp(seconds))
+                    else:
+                        reviewNote = data[-1]
+                    PartnerId = DB.getPartnerId(ProgID, 'meliuz@zanox.com')
+                    line.extend((ProgID, Fdate, str(data[2]), 'meliuz@zanox.com', 'BRL', float(data[3]), 0 ,str(reviewNote),PartnerId,3,4 ))
+                    print(line)
+                    writer.writerow(line)
+            
     
         
         
         
                 
 
-write([['Teste', '12-01-2018', 32423412.0, '423'],['123 Milhas', 43036.0, '7O1-M0K-17', 2450.44],['Nescafé Dolce Gusto',43036.0, 5501915888.0, 646.0],['Monte Carlo Joias',43036.0, '772940652580-01', 96.0],  ['Nescafé Dolce Gusto',43036.0, 5501915888.0, 666.0],['Monte Carlo Joias',43036.0, '772940652580-01', 96.0], ['Motorola',43036.0, '01-64886397', 1300.0],['Monte Carlo Joias',43036.0, '772940652580-01', 96.0]])
+write([['Teste', '12-01-2018', 32423412.0, '423'],['Teste', '12-01-2018', 3242343412.0, '423'],['123 Milhas', 430536.0, '7O1-M0K-17', 2450.44],['Nescafé Dolce Gusto',43036.0, 5501915888.0, 646.0],['Monte Carlo Joias',43036.0, '7729406525580-01', 42.0],  ['Nescafé Dolce Gusto',43036.0, 5501915888.0, 666.0],['Monte Carlo Joias',43036.0, '772940652580-01', 142.0], ['Motorola',43036.0, '01-64886397', 1300.0],['Monte Carlo Joias',43036.0, '772940623452580-01', 96.0]])
